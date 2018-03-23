37033db2dd86b2d7998d2c6cfce5da789847727b
starwavelin <benjaminxianlin@gmail.com>
2018-03-23 17:20:00 -0400

    connection to mLab mongodb fine; polished problemService using ProblemModel; tesing the 3 endpoints lg

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const mongoose = require('mongoose');
+
 const logger = require('./log');
 const restRouter = require('./routes/restRouter');
 
+const dbConnString = fs.readFileSync('./configs/dbConn.txt', 'utf8');
+mongoose.connect(dbConnString);
+
+
 app.use('/api/v1', restRouter);
 
-app.listen(3000, () => logger.debug('Codepad started'));
+app.listen(3000, () => logger.debug('Codepad started: '));
--- server/services/problemService.js
+++ server/services/problemService.js
@@ -1,35 +1,6 @@
-let problems = [
-    {
-      "id":1,
-      "name":"Two Sum",
-      "desc":"Given an array of integers, find two numbers such that they add up to a specific target number.\n\nThe function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are NOT zero-based.",
-      "difficulty":"easy"
-    },
-    {
-      "id":2,
-      "name":"3Sum",
-      "desc":"Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.",
-      "difficulty":"medium"
-    },
-    {
-      "id":3,
-      "name":"4Sum",
-      "desc":"Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target?\n\nFind all unique quadruplets in the array which gives the sum of target.",
-      "difficulty":"medium"
-    },
-    {
-      "id":4,
-      "name":"Triangle Count",
-      "desc":"Given an array of integers, how many three numbers can be found in the array, so that we can build an triangle whose three edges length is the three numbers that we find?",
-      "difficulty":"hard"
-    },
-    {
-      "id":5,
-      "name":"Sliding Window Maximum",
-      "desc":"Given an array of n integer with duplicate number, and a moving window(size k), move the window at each iteration from the start of the array, find the maximum number inside the window at each moving.",
-      "difficulty":"super"
-    }
-  ];
+const logger = require('../log');
+
+const ProblemModel = require('../models/problemModel');
 
 /* Error messages */
 const problemDuplicationError = 'Problem already exists!';
@@ -38,25 +9,47 @@ const problemDuplicationError = 'Problem already exists!';
 const getProblems = function() {
     /* async call to grab problems from DB so using Promise */
     return new Promise((resolve, reject) => {
-        resolve(problems);
+        
+        ProblemModel.find({}, function(err, problems) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(problems);
+            }
+        });
     });
 };
 
-const getProblem = function(id) {
+const getProblem = function(targetId) {
     return new Promise((resolve, reject) => {
-        resolve(problems.find(problem => problem.id === id));
+        ProblemModel.findOne({id:targetId}, function(err, problem) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(problem);
+            }
+        });
     });
 }
 
 const addProblem = function(newProblem) {
     return new Promise((resolve, reject) => {
-        if (problems.find(problem => problem.name === newProblem.name)) {
-            reject(problemDuplicationError);
-        } else {
-            newProblem.id = problems.length + 1;
-            problems.push(newProblem);
-            resolve(newProblem);
-        }
+        // check if newProblem is an already existing one
+        ProblemModel.findOne({name: newProblem.name}, function(err, data) {
+            if (err) {
+                reject(err);
+            } else if (data) {
+                reject(problemDuplicationError);
+            } else {
+                //save newProblem to mongodb
+                ProblemModel.count({}, function(err, len) {
+                    newProblem.id = len + 1;
+                    let mongoProblem = new ProblemModel(newProblem);
+                    mongoProblem.save();
+                    resolve(mongoProblem);
+                })
+            }
+        });
     });
 }
 
