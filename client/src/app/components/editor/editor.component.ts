4330f8d2d74a0a28f21e8608a33e42333638d278
starwavelin <benjaminxianlin@gmail.com>
2018-03-25 15:59:47 -0400

    editor for java done

--- client/src/app/components/editor/editor.component.ts
+++ client/src/app/components/editor/editor.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var ace: any;
+
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorComponent implements OnInit {
 
+  editor: any;
+
+  defaultContent = {
+    'Java':
+    `public class Solution {
+  public static void main(String[] args) {
+    //Type your code here
+  }
+}`
+  };
+
   constructor() { }
 
   ngOnInit() {
+    this.editor = ace.edit('editor');
+    this.editor.setTheme('ace/theme/eclipse');
+    this.editor.session.setMode('ace/mode/java');
+
+    this.editor.setValue(this.defaultContent['Java']);
+
   }
 
 }
