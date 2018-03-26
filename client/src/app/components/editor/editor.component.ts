50ad9724aa25a5f83adfef83440f897f4d248687
starwavelin <benjaminxianlin@gmail.com>
2018-03-26 11:21:27 -0400

    enhanced code editor by supplying language selection, reset and submit buttons; added python support

--- client/src/app/components/editor/editor.component.ts
+++ client/src/app/components/editor/editor.component.ts
@@ -9,15 +9,22 @@ declare var ace: any;
 })
 export class EditorComponent implements OnInit {
 
+  languages: string[] = ['Java', 'Python'];
+  language: string = 'Java';
   editor: any;
 
   defaultContent = {
     'Java':
     `public class Solution {
   public static void main(String[] args) {
-    //Type your code here
+    // Type your code here
   }
-}`
+}`,
+  'Python':
+`class Solution:
+  def main():
+    # Write your code here
+`,
   };
 
   constructor() { }
@@ -25,10 +32,26 @@ export class EditorComponent implements OnInit {
   ngOnInit() {
     this.editor = ace.edit('editor');
     this.editor.setTheme('ace/theme/eclipse');
-    this.editor.session.setMode('ace/mode/java');
+    this.resetEditor();
+  }
 
-    this.editor.setValue(this.defaultContent['Java']);
+  /**
+   * Triggered when user picks a language in the language dropdown 
+   * @param lang 
+   */
+  setLanguage(lang: string): void {
+    this.language = lang;
+    this.resetEditor();
+  } 
 
+  resetEditor(): void {
+    this.editor.session.setMode(`ace/mode/${this.language.toLowerCase()}`);
+    this.editor.setValue(this.defaultContent[this.language]);
+  }
+
+  submit(): void {
+    let userCode = this.editor.getValue();
+    console.log(`Your input code is:\n ${userCode}`); // log user code for now
   }
 
 }
